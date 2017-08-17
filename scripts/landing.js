ba3b5a6df443b79c81578127d69b30845fc5ad0d
beth <beth@MacBook-Pro.local>
2017-08-17 16:26:29 -0500

    Assignment Revisions 2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,13 @@
-var revealPoint = function(){
-  var points = document.getElementsByClassName('point');
-  for (var i = 0; i < points.length; i++){
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-};
-revealPoint();
+var animatePoints = function() {
 
-window.onload = function(){
-  window.addEventListener('scroll', function(event){
-    console.log(event);
-  });
-}
+  var revealPoint = function(){
+  var points = document.getElementsByClassName('point');
+    for (var i = 0; i < points.length; i++){
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+  };
+  revealPoint();
+};
