8b56da63b6c0522d948112d7309f91d2fcecc4da
beth <beth@MacBook-Pro.local>
2017-08-27 19:09:38 -0500

    fixing wrong files

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,26 +1,3 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
-
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-
-  function revealPoint(){
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-      function forEach(){
-        points.forEach(index);
-      }
-      forEach(revealPoint, index)
-};
-
-//var pointsArray = document.getElementsByClassName('point');
-
-//var animatePoints = function(points) {
 function forEach(points, callBackFn){
   for (var i=0; i<points.length; i++){
      callBackFn(points[i]);
